2fd76e67a58f83f0c443a61137d6e6d35b1a8e73
Jaron Swab <jrswab@gmail.com>
2019-05-21 22:04:46 -0400

    Total Gibs of free memory added

--- status.go
+++ status.go
@@ -1,18 +1,41 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 )
 
 func main() {
+	i := 0
 
-	for true {
-		hTime := time.Now().Format("2006-01-02 15:04:05")
-		//fmt.Print(hTime.String(), "\n")
-		cmd := exec.Command("xsetroot", "-name", hTime)
+	for i < 3700 {
+		hTime := time.Now().Format("Jan 02, 2006 15:04")
+
+		// get free Ram using Posix compliant shell command
+		ramCmd := "free -h | gawk '/Mem:/ {print $4}'"
+		ramGib, err := exec.Command("sh", "-c", ramCmd).Output()
+		if err != nil {
+			fmt.Println(err)
+		}
+		ramFree := strings.TrimSpace(string(ramGib))
+
+		// storing desired items as strings
+		cat := []string{"RAM:", ramFree,
+			"free", "|", hTime}
+
+		// concatinate all strings to one line for output
+		status := strings.Join(cat, " ")
+
+		cmd := exec.Command("xsetroot", "-name", status)
 		cmd.Run()
 		time.Sleep(1 * time.Second)
+
+		if i > 3600 {
+			i = 0
+		} else {
+			i++
+		}
 	}
 }
