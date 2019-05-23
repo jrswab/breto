a58b6d7274c9bd8066e2317ef70209ca2399f129
Jaron Swab <jrswab@gmail.com>
2019-05-23 19:18:34 -0400

    moved functions to their own libraries Changed the weather output to remove emoji Set array to make it easier to comment out or remove undesired 'modules'

--- main.go
+++ main.go
@@ -1,60 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"./libs/ram"
+	"./libs/wttr"
 	"os/exec"
 	"strings"
 	"time"
 )
 
-func getWttr() string {
-	wttrCmd := "curl -s 'wttr.in/?format=2'"
-	wttrRun, err := exec.Command("sh", "-c", wttrCmd).Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return strings.TrimSpace(string(wttrRun))
-}
-
-func getRam() string {
-	ramCmd := "free -h | gawk '/Mem:/ {print $4}'"
-	ramGib, err := exec.Command("sh", "-c", ramCmd).Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return strings.TrimSpace(string(ramGib))
-}
-
 func main() {
 	// initial run
 	i := 0
-	wttr := getWttr()
-	ramFree := getRam()
+	weather := wttr.Local()
+	ramFree := ram.Free()
 
 	for i < 3700 {
 		hTime := time.Now().Format("Jan 02, 2006 15:04")
-
+		// time delayed retrievals:
 		// get weather once per hour
 		if i == 3600 {
-			wttr = getWttr()
+			weather = wttr.Local()
 		}
-
 		// get free Ram every 3 seconds
 		if i%3 == 0 {
-			ramFree = getRam()
+			ramFree = ram.Free()
 		}
 
-		// storing desired items as strings
-		cat := []string{"RAM:", ramFree, "free", "|", wttr, "|", hTime}
+		// store desired items as strings
+		// delete or comment out a line to remove from status bar
+		cat := []string{" ",
+			"RAM:", ramFree, "free", "|",
+			weather, "|",
+			hTime}
 
 		// concatinate all strings to one line for output
 		status := strings.Join(cat, " ")
 
 		cmd := exec.Command("xsetroot", "-name", status)
 		cmd.Run()
+
 		time.Sleep(1 * time.Second)
 
-		// reset i to loop wttr updates
+		// reset i to loop for time delayed updates
 		if i > 3600 {
 			i = 0
 		} else {
