92ca00688a2b03dca635cc41f726fbcbfc1c25f3
jrswab <jrswab@gmail.com>
2019-05-29 11:21:35 -0400

    Moved to a 'ticker' instead of counting rounds with an 'i' variable. go routines now run after the time is set as the status. The chanels stay open to receive new messages and update with a switch statement to update the status as new data is avalible.

--- libs/ram/ram.go
+++ libs/ram/ram.go
@@ -4,29 +4,18 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
-	"time"
 )
 
 func Free(cRam chan string) {
 	ramFree := ""
-	for i := 0; i < 5; {
-		if i == 0 || i == 3 {
-			// set shell command
-			ramCmd := "free -h | gawk '/Mem:/ {print $4}'"
-			// run and save the output of ramCmd to ramGib
-			ramGib, err := exec.Command("sh", "-c", ramCmd).Output()
-			if err != nil {
-				cRam <- err.Error()
-			}
-			// return ramGib as string
-			ramFree = fmt.Sprintf("Ram: %s free | ", strings.TrimSpace(string(ramGib)))
-		}
-		cRam <- ramFree
-		if i == 3 {
-			i = 0
-		} else {
-			i++
-		}
-		time.Sleep(1 * time.Second)
+	// set shell command
+	ramCmd := "free -h | gawk '/Mem:/ {print $4}'"
+	// run and save the output of ramCmd to ramGib
+	ramGib, err := exec.Command("sh", "-c", ramCmd).Output()
+	if err != nil {
+		cRam <- err.Error()
 	}
+	// return ramGib as string
+	ramFree = fmt.Sprintf("Ram: %s free | ", strings.TrimSpace(string(ramGib)))
+	cRam <- ramFree
 }
--- libs/wttr/wttr.go
+++ libs/wttr/wttr.go
@@ -5,33 +5,21 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 )
 
-func Local(c chan string) {
-	weather := ""
-	for i := 0; i < 3636; {
-		// get temp(%t) and wind direction/speed (%w)
-		// for exact location add postal code - wttr.in/~15222?format...
-		// for more wttr options see https://wttr.in/:help
-		if i == 0 || i == 3600 {
-			resp, err := http.Get("https://wttr.in/?format=%t+%w")
-			if err != nil {
-				errMessage := "wttr connection issue"
-				c <- errMessage
-			}
-			defer resp.Body.Close() // close http request
-
-			// convert responce to string for return
-			bodyData, _ := ioutil.ReadAll(resp.Body)
-			weather = fmt.Sprintf("%s | ", strings.TrimSpace(string(bodyData)))
-		}
-		c <- weather
-		if i == 3600 {
-			i = 0
-		} else {
-			i++
-		}
-		time.Sleep(1 * time.Second)
+func Local(cWttr chan string) {
+	// get temp(%t) and wind direction/speed (%w)
+	// for exact location add postal code - wttr.in/~15222?format...
+	// for more wttr options see https://wttr.in/:help
+	resp, err := http.Get("https://wttr.in/?format=%t+%w")
+	if err != nil {
+		errMessage := "wttr connection issue"
+		cWttr <- errMessage
 	}
+
+	// convert responce to string for return
+	bodyData, _ := ioutil.ReadAll(resp.Body)
+	weather := fmt.Sprintf("%s | ", strings.TrimSpace(string(bodyData)))
+	cWttr <- weather
+	resp.Body.Close() // close http request
 }
