ec52b3d4d7bfdd4c2b591f1c0c0d37a1e49ed2b3
Jaron Swab <jrswab@gmail.com>
2019-05-28 22:05:45 -0400

    Updated libs to go routines. libs ram.go and wttr.go now hold the for loops and update the data passed over the chanel at their specified time. main.go no longe needs a counter in the for loop and loops for the entire length of the process. Removed points from wttr.go for the time being.

--- libs/wttr/wttr.go
+++ libs/wttr/wttr.go
@@ -1,24 +1,37 @@
 package wttr
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func Local() *string {
-	// get temp(%t) and wind direction/speed (%w)
-	// for exact location add postal code - wttr.in/~15222?format...
-	// for more wttr options see https://wttr.in/:help
-	resp, err := http.Get("https://wttr.in/?format=%t+%w")
-	if err != nil {
-		errMessage := "wttr connection issue"
-		return &errMessage
-	}
-	defer resp.Body.Close() // close http request
+func Local(c chan string) {
+	weather := ""
+	for i := 0; i < 3636; {
+		// get temp(%t) and wind direction/speed (%w)
+		// for exact location add postal code - wttr.in/~15222?format...
+		// for more wttr options see https://wttr.in/:help
+		if i == 0 || i == 3600 {
+			resp, err := http.Get("https://wttr.in/?format=%t+%w")
+			if err != nil {
+				errMessage := "wttr connection issue"
+				c <- errMessage
+			}
+			defer resp.Body.Close() // close http request
 
-	// convert responce to string for return
-	bodyData, _ := ioutil.ReadAll(resp.Body)
-	weather := strings.TrimSpace(string(bodyData))
-	return &weather
+			// convert responce to string for return
+			bodyData, _ := ioutil.ReadAll(resp.Body)
+			weather = fmt.Sprintf("%s | ", strings.TrimSpace(string(bodyData)))
+		}
+		c <- weather
+		if i == 3600 {
+			i = 0
+		} else {
+			i++
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
--- main.go
+++ main.go
@@ -4,45 +4,40 @@ import (
 	"fmt"
 	"gitlab.com/jrswab/dwm-status/libs/ram"
 	"gitlab.com/jrswab/dwm-status/libs/wttr"
-	"os/exec"
-	"strings"
+	//"os/exec"
 	"time"
 )
 
 func main() {
 	// initial run
-	var i uint16 = 0
-	weather := strings.TrimSpace(string(*wttr.Local()))
-	ramFree := ram.Free()
+	weather := ""
+	ramFree := ""
 
-	for i < 3700 {
+	// get weather data
+	c := make(chan string)
+	go wttr.Local(c)
+
+	// get free ram data
+	cRam := make(chan string)
+	go ram.Free(cRam)
+
+	for {
 		// to add seconds use "Jan 02, 2006 15:04:05"
 		hTime := time.Now().Format("Jan 02, 2006 15:04")
-		// time delayed retrievals:
-		// get weather once per hour
-		if i == 3600 {
-			weather = strings.TrimSpace(string(*wttr.Local()))
-		}
-		// get free Ram every 3 seconds
-		if i%3 == 0 {
-			ramFree = ram.Free()
-		}
 
 		// Change what is displayed by adding or removing variables (eg. weather).
 		// Change the text output by editing bewteen the `""`.
 		// Make sure to have the same number of `%s` as variables.
-		status := fmt.Sprintf(" RAM: %s free | %s | %s ", ramFree, weather, hTime)
+		status := fmt.Sprintf(" %s%s%s ", ramFree, weather, hTime)
 
-		cmd := exec.Command("xsetroot", "-name", status)
-		cmd.Run()
+		//cmd := exec.Command("xsetroot", "-name", status)
+		//cmd.Run()
+		fmt.Println(status)
 
 		time.Sleep(1 * time.Second)
 
-		// reset i to loop for time delayed updates
-		if i > 3600 {
-			i = 0
-		} else {
-			i++
-		}
+		// set channel data for next loop
+		ramFree = <-cRam
+		weather = <-c
 	}
 }
