c4c0ec8843d64ae07f5e1d96515f4a4da62a0722
Jaron Swab <jrswab@gmail.com>
2019-05-29 21:43:48 -0400

    Added tickers to go libs Removed the counters from the bottom of main.go and cleaned up comments Now each lib has a ticker and sends data to main ever 'n' seconds

--- libs/wttr/wttr.go
+++ libs/wttr/wttr.go
@@ -3,23 +3,34 @@ package wttr
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func Local(cWttr chan string) {
-	// get temp(%t) and wind direction/speed (%w)
-	// for exact location add postal code - wttr.in/~15222?format...
-	// for more wttr options see https://wttr.in/:help
-	resp, err := http.Get("https://wttr.in/?format=%t+%w")
-	if err != nil {
-		errMessage := "wttr connection issue"
-		cWttr <- errMessage
-	}
+	var passed, hour float64
+	start := time.Now() // to determine seconds passed
+	ticker := time.NewTicker(time.Second)
 
-	// convert responce to string for return
-	bodyData, _ := ioutil.ReadAll(resp.Body)
-	weather := fmt.Sprintf("%s | ", strings.TrimSpace(string(bodyData)))
-	cWttr <- weather
-	resp.Body.Close() // close http request
+	for range ticker.C {
+		passed = time.Since(start).Seconds() // total seconds passed
+		hour = math.Floor(math.Remainder(passed, 3600))
+		if passed < 10 || hour == 0 {
+			// get temp(%t) and wind direction/speed (%w)
+			// for exact location add postal code - wttr.in/~15222?format...
+			// for more wttr options see https://wttr.in/:help
+			resp, err := http.Get("https://wttr.in/?format=%t+%w")
+			if err != nil {
+				errMessage := "wttr connection issue"
+				cWttr <- errMessage
+			}
+
+			bodyData, _ := ioutil.ReadAll(resp.Body)
+			weather := fmt.Sprintf("%s | ",
+				strings.TrimSpace(string(bodyData))) // convert responce to string for return
+			cWttr <- weather
+		}
+	}
 }
