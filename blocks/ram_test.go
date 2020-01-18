c3037a46e4052f05dc191c6abfa6e0d15b5cc5ff
Jaron Swab <jrswab@gmail.com>
2020-01-17 20:20:02 -0500

    Updated RAM Test.
    Now it actually checks if the output is as expected. Not just printing
    to stdout.

--- blocks/ram_test.go
+++ blocks/ram_test.go
@@ -1,27 +1,38 @@
 package blocks
 
 import (
-	"fmt"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
 // A basic test to make sure the function runs
 // and outputs data instead of an error
-func TestFreeRam(t *testing.T) {
-	cRam := make(chan string)
-	eRam := make(chan error)
-	var ram string
+func TestFreeRAM(t *testing.T) {
+	cRAM := make(chan string)
+	eRAM := make(chan error)
+	var got string
 	var runError error
 
-	go FreeRam(cRam, eRam)
+	go FreeRam(cRAM, eRAM)
+
+	ramCmd := "free -h | gawk '/Mem:/ {print $7}'"             // set shell command
+	expected, err := exec.Command("sh", "-c", ramCmd).Output() // run and save the output
+	if err != nil {
+		eRAM <- err
+	}
 
 	select {
-	case ram = <-cRam:
-	case runError = <-eRam:
+	case got = <-cRAM:
+	case runError = <-eRAM:
 	}
 
 	if runError != nil {
 		t.Error("Expected Free Ram got:", runError.Error())
 	}
-	fmt.Println(ram)
+
+	if strings.TrimSpace(string(expected)) != got {
+		t.Errorf("\nExpected %s but got %s", expected, got)
+	}
+	return
 }
